c47f7c505e7014e1e787ab195d31dabf2b38e2a9
guisanoli <83303272+guisanoli@users.noreply.github.com>
2023-03-13 19:00:04 -0300

    Exercicios

---
+++ Lab 01b/ex10/main.c
@@ -0,0 +1,73 @@
+#include stdio.h
+#include time.h
+#include stdlib.h
+
+
+#define LINHA 5
+#define COLUNA 5
+
+int main() {
+    int matriz[LINHA][COLUNA];
+    int linhas=5, colunas=5, i, j, num, option;
+
+    for (i = 0; i  linhas; i++) {
+        for (j = 0; j  colunas; j++) {
+            matriz[i][j]=rand()%100;
+        }
+    }
+    printf(Matriz originaln);
+    for (i = 0; i  linhas; i++) {
+        for (j = 0; j  colunas; j++) {
+            printf(%d , matriz[i][j]);
+        }
+        printf(n);
+    }
+
+    printf(Digite 1 para multiplicar uma linha ou 2 para multiplicar uma coluna );
+    scanf(%d, &option);
+
+    printf(Digite o número pelo qual deseja multiplicar a linhacoluna );
+    scanf(%d, &num);
+
+    if (option == 1) {
+        int lin;
+
+        printf(Digite o número da linha que deseja multiplicar );
+        scanf(%d, &lin);
+
+         Multiplica a linha pelo número
+        for (j = 0; j  colunas; j++) {
+            matriz[lin][j] = num;
+        }
+
+         Imprime a matriz resultante
+        printf(Matriz resultanten);
+        for (i = 0; i  linhas; i++) {
+            for (j = 0; j  colunas; j++) {
+                printf(%d , matriz[i][j]);
+            }
+            printf(n);
+        }
+    } else if (option == 2) {
+        int col;
+
+        printf(Digite o número da coluna que deseja multiplicar );
+        scanf(%d, &col);
+
+        for (i = 0; i  linhas; i++) {
+            matriz[i][col] = num;
+        }
+
+        printf(Matriz resultanten);
+        for (i = 0; i  linhas; i++) {
+            for (j = 0; j  colunas; j++) {
+                printf(%d , matriz[i][j]);
+            }
+            printf(n);
+        }
+    } else {
+        printf(Opção inválida!n);
+    }
+
+    return 0;
+}
