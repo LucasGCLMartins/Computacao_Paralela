398ad0feec4d4e55e965d0133725d10543784909
Guilherme <guilherme.sanoliv@gmail.com>
2023-04-17 18:32:56 -0300

    atualizacao codigos

--- Lab 04/multSerial.c
+++ Lab 04/multSerial.c
@@ -1,34 +1,28 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-  
+
 #define M 3
 
-  
+int A[M][M] = { {1,2,3}, {4,5,6}, {7,8,9} };
+int B[M][M] = { {9,8,7}, {6,5,4}, {3,2,1} };
+int C[M][M];
 
-  
 int main()
 {
-  int mat1[M][M] = { { 1, 2, 3 }, {4,5,6 },{7,8,9 }};
-  
-    int mat2[M][M] = { {9,8,7 },{ 6, 5 ,4},{3,2,1 }};
-  
-  int rslt[M][M];
-  
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < M; j++) {
-            rslt[i][j] = 0;
-  
-            for (int k = 0; k < M; k++) {
-                rslt[i][j] += mat1[i][k] * mat2[k][j];
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            C[i][j] = 0;
+
+            for (int k = 0; k < M; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
             }
-  
-            printf("%d\t", rslt[i][j]);
+            printf("%d\t", C[i][j]);
         }
-  
         printf("\n");
     }
-  
     return 0;
 }
-  
+
--- Lab 04/multThreads.c
+++ Lab 04/multThreads.c
@@ -9,55 +9,63 @@ int A [M][M] = { {1,2,3}, {4,5,6}, {7,8,9} };
 int B [M][M] = { {9,8,7}, {6,5,4}, {3,2,1} };
 int C [M][M];
 
-struct v {
-   int i; /* row */
-   int j; /* column */
+struct v
+{
+    int i; /* Fileira */
+    int j; /* Coluna */
 };
 
-void *runner(void *param); /* the thread */
+//Programa usando as threads que multiplica cada elemento da fileira pelo da coluna
+void *runner(void *param)
+{
+    struct v *data = param; //Estrutura que armazena os dados
+    int n, sum = 0; //Contador e soma
 
-int main(int argc, char *argv[]) {
-   int i,j, count = 0;
-   for(i = 0; i < M; i++) {
-      for(j = 0; j < M; j++) {
-         //Assign a row and column for each thread
-         struct v *data = (struct v *) malloc(sizeof(struct v));
-         data->i = i;
-         data->j = j;
-         /* Now create the thread passing it data as a parameter */
-         pthread_t tid;       //Thread ID
-         pthread_attr_t attr; //Set of thread attributes
-         //Get the default attributes
-         pthread_attr_init(&attr);
-         //Create the thread
-         pthread_create(&tid,&attr,runner,data);
-         //Make sure the parent waits for all thread to complete
-         pthread_join(tid, NULL);
-         count++;
-      }
-   }
+    //Fileira multiplicada pela coluna
+    for(n = 0; n< M; n++)
+    {
+        sum += A[data->i][n] * B[n][data->j];
+    }
+    //Coloca a soma no local certo
+    C[data->i][data->j] = sum;
 
-   //Print out the resulting matrix
-   for(i = 0; i < M; i++) {
-      for(j = 0; j < M; j++) {
-         printf("%d ", C[i][j]);
-      }
-      printf("\n");
-   }
+    //Sai da thread
+    pthread_exit(0);
 }
 
-//The thread will begin control in this function
-void *runner(void *param) {
-   struct v *data = param; // the structure that holds our data
-   int n, sum = 0; //the counter and sum
+int main(int argc, char *argv[])
+{
+    int i,j, count = 0;
+    for(i = 0; i < M; i++)
+    {
+        for(j = 0; j < M; j++)
+        {
+            //Coloca uma fileira e uma coluna em cada thread
+            struct v *data = (struct v *) malloc(sizeof(struct v));
+            data->i = i;
+            data->j = j;
+            /* Cria a thread passando os seus dados como paramentro */
+            pthread_t tid;       //ID da Thread
+            pthread_attr_t attr; //Atributos da thread
+            //Atributos ininciais
+            pthread_attr_init(&attr);
+            //Cria a thread
+            pthread_create(&tid,&attr,runner,data);
+            //Processo pai espera
+            pthread_join(tid, NULL);
+            count++;
+        }
+    }
 
-   //Row multiplied by column
-   for(n = 0; n< M; n++){
-      sum += A[data->i][n] * B[n][data->j];
-   }
-   //assign the sum to its coordinate
-   C[data->i][data->j] = sum;
-
-   //Exit the thread
-   pthread_exit(0);
+    //Imprime a matriz
+    for(i = 0; i < M; i++)
+    {
+        for(j = 0; j < M; j++)
+        {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
 }
+
+
