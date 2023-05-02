8b0c596ab2a85a3d03f9d848079aa4ec5760043e
guisanoli <83303272+guisanoli@users.noreply.github.com>
2023-05-02 07:49:30 -0300

    Add files via upload

---
+++ PROJETO/eulerV2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<omp.h>
+
+void Euler(int n , double *global_result_p);
+double f(double x);
+
+int main(int argc, char *argv[]){
+        double global_result = 0.0;
+        int n=1000;
+        int thread_count;
+
+        thread_count = strtol(argv[1],NULL,10);
+
+        #pragma omp parallel num_threads(thread_count)
+        Euler(n,&global_result);;
+        printf("%.14e\n",n,a,b,global_result);
+        return 0;
+}
+void Euler(int n, double *global_result_p){
+        double x,my_result=0.0;
+        int i,local_n;
+        int my_rank = omp_get_thread_num();
+        int thread_count = omp_get_num_threads();
+
+        local_n = n/thread_count;
+
+        for(i=0;i<=local_n;i++){
+            my_result+=1/(f(i));
+        }
+
+        #pragma omp critical
+        *global_result_p += my_result;
+}
+float f(int n)  
+{  
+  if (n == 0)  
+    return 1;  
+  else  
+    return(n * f(n-1));  
+}
